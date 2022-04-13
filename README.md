fcc7a4cda933969a482c1aebebc088978cb07cfb
Darrion Mills <dmills1994@gmail.com>
2022-04-13 12:19:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shopping Cart
+
+Mock Shopping Cart UI built in React.
+
+## Built With
+- HTML
+- CSS
+- JavaScript
+- React
+- React Router
+- FontAwesome
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* <Route path="/products" element={} />  */}
+        {/* <Route path="/products/:id" element={} /> */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
